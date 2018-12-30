80743ea2422c898f21eda5533bb2b60ec152043c
MirTown <mk91game@gmail.com>
2018-12-30 10:09:34 +0500

    added models

---
+++ src/model/BlogPost.java
@@ -0,0 +1,123 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+public class BlogPost {
+    private Long id;
+    private String title;
+    private String post;
+    private User author;
+    private LocalDateTime date;
+    private Set<Tag> tags;
+    private Set<Comment> comments;
+    private Set<User> likes;
+    private Set<User> dislikes;
+
+    @Override
+    public String toString() {
+        return "BlogPost{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", post='" + post + '\'' +
+                ", author=" + author +
+                ", date=" + date +
+                ", tags=" + tags +
+                ", comments=" + comments +
+                ", likes=" + likes +
+                ", dislikes=" + dislikes +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public Set<User> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Set<User> likes) {
+        this.likes = likes;
+    }
+
+    public Set<User> getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(Set<User> dislikes) {
+        this.dislikes = dislikes;
+    }
+
+    public BlogPost() {
+
+    }
+
+    public BlogPost(Long id, String title, String post, User author, LocalDateTime date, Set<Tag> tags, Set<Comment> comments, Set<User> likes, Set<User> dislikes) {
+
+        this.id = id;
+        this.title = title;
+        this.post = post;
+        this.author = author;
+        this.date = date;
+        this.tags = tags;
+        this.comments = comments;
+        this.likes = likes;
+        this.dislikes = dislikes;
+    }
+}
+
+
---
+++ src/model/Comment.java
@@ -0,0 +1,88 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public class Comment {
+    private Long id;
+    private User user;
+    private String message;
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", user=" + user +
+                ", message='" + message + '\'' +
+                ", date=" + date +
+                ", likes=" + likes +
+                ", dislikes=" + dislikes +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Set<User> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Set<User> likes) {
+        this.likes = likes;
+    }
+
+    public Set<User> getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(Set<User> dislikes) {
+        this.dislikes = dislikes;
+    }
+
+    public Comment() {
+
+    }
+
+    public Comment(Long id, User user, String message, LocalDateTime date, Set<User> likes, Set<User> dislikes) {
+
+        this.id = id;
+        this.user = user;
+        this.message = message;
+        this.date = date;
+        this.likes = likes;
+        this.dislikes = dislikes;
+    }
+
+    private LocalDateTime date;
+    private Set<User> likes;
+    private Set<User> dislikes;
+}
